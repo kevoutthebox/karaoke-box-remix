cabb23322f1baf36ab82dc7b2f40dfb6247e7517
kevinl <kevinliu987@gmail.com>
2016-10-10 23:53:48 -0700

    setup action creators, reducers, and redux form(signup page)

--- public/src/components/signup.jsx
+++ public/src/components/signup.jsx
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actionCreators from '../actions';
 
 class Signup extends Component {
+  handleFormSubmit(formProps) {
+    // Call signUpUser action creator to sign up and store in DB
+    this.props.signUpUser(formProps);
+  }
+
   render() {
-    return(
-      <div>
-        Please sign up and sign!
-      </div>
+    const { handleSubmit, fields: { email, password, passwordConfirmation } } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className="form-group">
+          <label>Email Address:</label>
+          <input className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password</label>
+          <input className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password Confirmation</label>
+          <input className="form-control" />
+        </fieldset>
+
+        <button className="btn btn-primary" type="submit">Sign me up!</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+function validate(formProps) {
+  const errors = {};
+
+  return errors;
+}
+
+function mapStateToProps(state) {
+  return { errorMsg: state.auth.error };
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirmation'],
+  validate
+}, mapStateToProps, actionCreators)(Signup);
